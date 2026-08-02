e6c4cdd0bb4c28b9fcb73d5efa6311dbbda16c5b
migueltrejo2899 <miguel.tcg28@gmail.com>
2026-08-02 11:56:13 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '25mb',
+    },
+  },
+};
 module.exports = nextConfig;
